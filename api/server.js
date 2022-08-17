d35e60a7cb98bd442bdd90695385696a794bdb08
unknown <edev99999@gmail.com>
2022-08-17 16:56:33 -0400

    Complete Backend

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 4000;
+
+const data = require('./db');
+const createTournament = require('./createTournament');
+const dbroute = require('./router');
+
+// Database Initializing
+for (let i = 0; i < 3; i++) {
+  data.tournaments.push(createTournament());
+}
+console.log('Database Initialized!');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Server is Running');
+});
+
+// Router
+app.use('/database', dbroute);
+
+app.listen(PORT, () => {
+  console.log(`Server running on localhost:4000`);
+});
--- api/timeout.js
+++
@@ -1,3 +0,0 @@
-module.exports = (_req, _res, next) => {
-  setTimeout(next, 10000);
-};
