b388fa1c98bb5677cc44781c8719fffde3ce3957
chassitynakamura <nakamurachassity@gmail.com>
2022-08-18 15:36:52 -0700

    updated gitignore

--- dist/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-// Internal Dependencies
-const Button_Styled_1 = require("./Button.Styled");
-const Button = ({ children, handleClick }) => {
-    return (0, jsx_runtime_1.jsx)(Button_Styled_1.StyledButton, Object.assign({ onClick: handleClick }, { children: children }));
-};
-exports.default = Button;
--- dist/components/Input/Input.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-// Internal Dependencies
-const Input_Styled_1 = require("./Input.Styled");
-const Input = ({ handleChange }) => {
-    return ((0, jsx_runtime_1.jsx)(Input_Styled_1.StyledInput, { placeholder: "Search tournament...", onChange: handleChange }));
-};
-exports.default = Input;
